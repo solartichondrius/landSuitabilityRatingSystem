cf88815b32c96942518b38ab51fa2b83286c2226
Thomas Curtis <thomas.curtis@canada.ca>
2020-01-22 16:08:31 -0500

    added soilRatingClass and soilRatingPoints (not finished)

---
+++ landSuitabilityRatingSystem/soilFactors/soilRatingClass.R
@@ -0,0 +1,5 @@
+# Title     : Soil Rating Class
+# Objective :
+# Created by: CurtisTh
+# Created on: 2020-01-22
+
---
+++ landSuitabilityRatingSystem/soilFactors/soilRatingPoints.R
@@ -0,0 +1,10 @@
+# Title     : Soil Rating Points
+# Objective : Calculate the soil rating
+# Created by: CurtisTh
+# Created on: 2020-01-22
+
+soilRatingPoints <- function(EGDD){
+  Z <- soilTemperature(EGDD)
+  a <- 100 - Z
+
+}
