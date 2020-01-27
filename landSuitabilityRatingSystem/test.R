cdcf7b7bb1ab0a04b0521c0ca14a059a2b049dd8
Thomas Curtis <thomas.curtis@canada.ca>
2020-01-27 11:59:07 -0500

    created seperate results functions for soil,landscape,and climate; and created a results function which combines the 3

--- landSuitabilityRatingSystem/test.R
+++ landSuitabilityRatingSystem/test.R
@@ -3,4 +3,4 @@
 # Created by: CurtisTh
 # Created on: 2020-01-21
 
-csvResults("O:/Temporary/lsrs/test_data/ab_vector/climate1981x10_CCCS_baseline.csv","O:/Temporary/lsrs/test_data/ab_vector/climate1981x10_CCCS_baseline_testResults.csv")
+results("O:/Temporary/lsrs/test_data/ab_vector/climate1981x10_CCCS_baseline.csv","O:/Temporary/lsrs/test_data/ab_vector/climate1981x10_CCCS_baseline_testResults.csv")
