c24dcc5c1308cbba23cbae06eee8cba9f6028667
CurtisTh <curt0053@algonquinlive.com>
2020-02-03 13:43:09 -0500

    updated percent progress in climate results

--- landSuitabilityRatingSystem/climaticFactors/climateResults.R
+++ landSuitabilityRatingSystem/climaticFactors/climateResults.R
@@ -6,8 +6,9 @@
 climateResults <- function(input,output,save=TRUE){
 
   df <- read.csv(input) #put the data from the CSV file into a dataframe
+  percent <- 0 #percent completion of this program, starting at 0 and going to 100
   for(i in 1:nrow(df)){ #loop through every row and assign a value to the climate rating class
-    results <- climateRating(df$ppe[i],df$esm[i],df$efm[i],df$egdd[i],df$eff[i])
+    results <- climateRating(df$ppe[i],df$esm[i],df$efm[i],df$egdd[i],df$eff[i]) #put the values from the loaded file into the climate rating function to calculate the results
     df$moisturePointDeduction[i] <- results[3]
     df$temperaturePointDeduction[i] <- results[4]
     df$basicClimateRating[i] <- results[5]
@@ -16,7 +17,10 @@ climateResults <- function(input,output,save=TRUE){
     df$fallFrostPercentDeduction[i] <- results[8]
     df$finalRatingPoints[i] <- results [2]
     df$climateRatingClass[i] <- results[1]
-    print(paste0(i/nrow(df)*100,"% complete")) #print out the current progress as a percentage
+    if(percent != floor(i/nrow(df)*100)){ #only update the percent progress if the new percent is not the same as the old percent (so it will only display a maximum of 100 lines rather than one for every single row in the dataframe
+      percent <- floor(i/nrow(df)*100) #calculate the percent progress which is the current row divided by all rows times 100
+      print(paste0(percent,"% complete")) #print out the current progress as a percentage
+    }
   }
   if(save==TRUE){ #if the save argument is set to TRUE (which it is by default)
     write.csv(df,output) #then write the dataframe to a file
---
+++ landSuitabilityRatingSystem/landscapeFactors/landscapeRating.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: CurtisTh
+# Created on: 1/31/2020
+
