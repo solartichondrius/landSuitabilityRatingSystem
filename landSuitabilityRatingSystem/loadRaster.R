28d276952cc783b1034047e784538980fb9c8635
CurtisTh <curt0053@algonquinlive.com>
2020-02-04 12:57:52 -0500

    new function to load raster files into a dataframe and remove NA (null) values

---
+++ landSuitabilityRatingSystem/loadRaster.R
@@ -0,0 +1,10 @@
+# Title     : Load Raster
+# Objective : Load Raster File into a Data Frame
+# Created by: CurtisTh
+# Created on: 2/4/2020
+
+loadRaster <- function(rasterFile){ #takes a raster file as an argument
+  library(raster) #load the raster library
+  rasterAsDF <- na.omit(as.data.frame(raster(rasterFile))) #create data frame created from raster removing NAs (null values)
+  return(rasterAsDF) #return the dataframe
+}
