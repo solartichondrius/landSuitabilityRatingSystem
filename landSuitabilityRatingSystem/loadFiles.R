ebfebb9e1b6cf0dbcb6fb430e8a0a5f8e0ad2e02
CurtisTh <curt0053@algonquinlive.com>
2020-02-04 12:57:04 -0500

    added option whether or not to "echo" (show the files being loaded)

--- landSuitabilityRatingSystem/loadFiles.R
+++ landSuitabilityRatingSystem/loadFiles.R
@@ -3,17 +3,17 @@
 # Created by: CurtisTh
 # Created on: 1/30/2020
 
-loadFiles <- function(...){ #can take any number of file names as arguments
+loadFiles <- function(...,ECHO=TRUE){ #can take any number of file names as arguments
   files <- c(...) #put all of the file names into a list
   for(file in files){ #for every file in the list of files
-    loadFile(file)
+    loadFile(file,ECHO)
   }
 }
 
-loadFile <- function(file){
+loadFile <- function(file,ECHO=TRUE){
   fileName <- sub("\\..*","",sub(".*/","",file)) #get the name of the file without the preceding folder path and .extension type
   f <- find(fileName) #try to find the file
   if(!identical(f,".GlobalEnv")){ #if the file is not already loaded
-    source(file,echo=TRUE) #load the file
+    source(file,echo=ECHO) #load the file
   }
 }
