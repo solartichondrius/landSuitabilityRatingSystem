314168bca7c5ea1932109bc8e1eeb3874c3eaa37
solartichondrius <curt0053@algonquinlive.com>
2020-02-06 14:38:19 -0500

    added some code to record how long it takes to completely process the large raster file

--- landSuitabilityRatingSystem/test.R
+++ landSuitabilityRatingSystem/test.R
@@ -10,6 +10,7 @@ source("All.R")
 #  landscapeIN="O:/Temporary/lsrs/test_data/ab_vector/landscapeTest2.csv",
 #  landscapeOUT="O:/Temporary/lsrs/test_data/ab_vector/landscapeTest2_vector.csv"
 #)
+startTime <- Sys.time()
 results(climateOUT="O:/Temporary/lsrs/test_data/ab_vector/climate1981x10_CCCS_baseline_testResults_raster.csv",
   climateIN=c("O:/Temporary/lsrs/test_data/ab_raster/AB_PPE_250m_lcc.tif",
             "O:/Temporary/lsrs/test_data/ab_raster/AB_PPE_250m_lcc.tif",
@@ -17,3 +18,6 @@ results(climateOUT="O:/Temporary/lsrs/test_data/ab_vector/climate1981x10_CCCS_ba
             "O:/Temporary/lsrs/test_data/ab_raster/AB_EGDD_250m_lcc.tif",
             "O:/Temporary/lsrs/test_data/ab_raster/Alberta_250mslc32grid_lcc.tif")
 )
+endTime <- Sys.time()
+timeTaken <- endTime - startTime
+print(timeTaken)
