148b8bd2d266d3c81a480235c98403f31d41a435
mcad0024 <55120328+mcad0024@users.noreply.github.com>
2020-02-27 11:55:57 -0500

    Delete website.R

--- landSuitabilityRatingSystem/website/website.R
+++
@@ -1,7 +0,0 @@
-# Title     : Website
-# Objective : Make a webpage for the Land Suitability Rating System
-# Created by: CurtisTh
-# Created on: 2020-02-10
-
-library(shiny)
-shinyApp(ui=ui,server=server)
